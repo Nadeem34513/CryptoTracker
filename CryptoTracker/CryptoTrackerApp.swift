92f708f573cc93e5f99d4bc99bc90dfe0b8dfa54
Nadeem Noushad <nadeembinnoushad12@gmail.com>
2024-09-07 12:27:31 +0530

    Initial Commit

---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by Nadeem Noushad on 07/09/24.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
