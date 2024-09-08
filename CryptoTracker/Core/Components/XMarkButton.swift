53590ad4aae0d54bb1a93e16a2b2b47bb1944634
Nadeem Noushad <nadeembinnoushad12@gmail.com>
2024-09-09 02:43:10 +0530

    feat: extension to include keyboard dismissal

---
+++ CryptoTracker/Core/Components/XMarkButton.swift
@@ -0,0 +1,18 @@
+//
+//  XMarkButton.swift
+//  CryptoTracker
+//
+//  Created by Nadeem Noushad on 09/09/24.
+//
+
+import SwiftUI
+
+struct XMarkButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    XMarkButton()
+}
---
+++ CryptoTracker/Core/Home/Views/PortfolioView.swift
@@ -0,0 +1,18 @@
+//
+//  PortfolioView.swift
+//  CryptoTracker
+//
+//  Created by Nadeem Noushad on 09/09/24.
+//
+
+import SwiftUI
+
+struct PortfolioView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PortfolioView()
+}
